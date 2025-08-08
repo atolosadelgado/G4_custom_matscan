9c9404d12b4b808e40fe4d3bcd2f540d877a8e73
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2025-08-08 13:20:41 +0200

    remove hardcoded configuration of gun, so UI commands can be used

--- src/G01PrimaryGeneratorAction.cc
+++ src/G01PrimaryGeneratorAction.cc
@@ -50,7 +50,7 @@ G01PrimaryGeneratorAction::G01PrimaryGeneratorAction()
   G4String particleName;
   fParticleGun->SetParticleDefinition(particleTable->FindParticle(particleName = "geantino"));
   fParticleGun->SetParticleEnergy(1.0 * GeV);
-  fParticleGun->SetParticlePosition(G4ThreeVector(-2.0 * m, 0.1, 0.1));
+  fParticleGun->SetParticlePosition(G4ThreeVector(0.0 * m, 0.0, 0.0));
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -64,18 +64,18 @@ G01PrimaryGeneratorAction::~G01PrimaryGeneratorAction()
 
 void G01PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-  G4int i = anEvent->GetEventID() % 3;
-  G4ThreeVector v(1.0, 0.0, 0.0);
-  switch (i) {
-    case 0:
-      break;
-    case 1:
-      v.setY(0.1);
-      break;
-    case 2:
-      v.setZ(0.1);
-      break;
-  }
-  fParticleGun->SetParticleMomentumDirection(v);
+  // G4int i = anEvent->GetEventID() % 3;
+  // G4ThreeVector v(1.0, 0.0, 0.0);
+  // switch (i) {
+  //   case 0:
+  //     break;
+  //   case 1:
+  //     v.setY(0.1);
+  //     break;
+  //   case 2:
+  //     v.setZ(0.1);
+  //     break;
+  // }
+  // fParticleGun->SetParticleMomentumDirection(v);
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
